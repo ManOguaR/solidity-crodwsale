75fc8fd6949588e89e131be3feeeaeb3f0af7dd3
Eduardo Ridao <manoguar@msn.com>
2023-10-07 04:30:19 +0200

    upgraded mintable.sol

--- crowdsale/abstractions/mintable.sol
+++ crowdsale/abstractions/mintable.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 
 interface IMintable {
     function mint(address to, uint256 amount) external returns (bool);
 }
 
-abstract contract ERC20Mintable is ERC20, IMintable, AccessControl {
+abstract contract ERC20Mintable is Context, ERC20, IMintable, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor(address defaultAdmin, address minter)
